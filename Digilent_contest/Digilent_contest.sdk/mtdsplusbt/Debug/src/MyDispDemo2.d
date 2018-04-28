33be1e95dcd459e35c95cc6680719a6cc9a1bf9c
Vlad Razvan-Andrei <vlad.razvan50@yahoo.com>
2018-04-28 09:55:04 +0300

    mtds+bluetooth

---
+++ Digilent_contest/Digilent_contest.sdk/mtdsplusbt/Debug/src/MyDispDemo2.d
@@ -0,0 +1,43 @@
+src/MyDispDemo2.o src/MyDispDemo2.d: ../src/MyDispDemo2.cc \
+ ../../mtdsplusbt_bsp/microblaze_0/include/MyDisp.h \
+ ../../mtdsplusbt_bsp/microblaze_0/include/mtds.h \
+ ../../mtdsplusbt_bsp/microblaze_0/include/MtdsDefs.h \
+ ../../mtdsplusbt_bsp/microblaze_0/include/MtdsHal.h \
+ ../../mtdsplusbt_bsp/microblaze_0/include/xparameters.h \
+ ../../mtdsplusbt_bsp/microblaze_0/include/xtmrctr.h \
+ ../../mtdsplusbt_bsp/microblaze_0/include/xil_types.h \
+ ../../mtdsplusbt_bsp/microblaze_0/include/xil_assert.h \
+ ../../mtdsplusbt_bsp/microblaze_0/include/xstatus.h \
+ ../../mtdsplusbt_bsp/microblaze_0/include/xtmrctr_l.h \
+ ../../mtdsplusbt_bsp/microblaze_0/include/xil_io.h \
+ ../../mtdsplusbt_bsp/microblaze_0/include/mb_interface.h \
+ ../../mtdsplusbt_bsp/microblaze_0/include/xil_exception.h \
+ ../../mtdsplusbt_bsp/microblaze_0/include/xil_printf.h
+
+../../mtdsplusbt_bsp/microblaze_0/include/MyDisp.h:
+
+../../mtdsplusbt_bsp/microblaze_0/include/mtds.h:
+
+../../mtdsplusbt_bsp/microblaze_0/include/MtdsDefs.h:
+
+../../mtdsplusbt_bsp/microblaze_0/include/MtdsHal.h:
+
+../../mtdsplusbt_bsp/microblaze_0/include/xparameters.h:
+
+../../mtdsplusbt_bsp/microblaze_0/include/xtmrctr.h:
+
+../../mtdsplusbt_bsp/microblaze_0/include/xil_types.h:
+
+../../mtdsplusbt_bsp/microblaze_0/include/xil_assert.h:
+
+../../mtdsplusbt_bsp/microblaze_0/include/xstatus.h:
+
+../../mtdsplusbt_bsp/microblaze_0/include/xtmrctr_l.h:
+
+../../mtdsplusbt_bsp/microblaze_0/include/xil_io.h:
+
+../../mtdsplusbt_bsp/microblaze_0/include/mb_interface.h:
+
+../../mtdsplusbt_bsp/microblaze_0/include/xil_exception.h:
+
+../../mtdsplusbt_bsp/microblaze_0/include/xil_printf.h:
